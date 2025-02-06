18592ba42a4d6f2612c4dac31fc71e1717551326
Franco Durand <francoduve.25@gmail.com>
2025-02-06 12:46:07 -0500

    feat: format creation date in PetResource and update assembler to handle date formatting

--- src/main/java/com/estrellaticona/vetcare/pets/interfaces/rest/resources/PetResource.java
+++ src/main/java/com/estrellaticona/vetcare/pets/interfaces/rest/resources/PetResource.java
@@ -1,13 +1,11 @@
 package com.estrellaticona.vetcare.pets.interfaces.rest.resources;
 
-import java.util.Date;
-
 public record PetResource(
-                Long id,
-                Long clientId,
-                String name,
-                char gender,
-                Integer age,
-                String specie,
-                Date register) {
+        Long id,
+        Long clientId,
+        String name,
+        char gender,
+        Integer age,
+        String specie,
+        String register) {
 }
--- src/main/java/com/estrellaticona/vetcare/pets/interfaces/rest/transform/PetResourceFromEntityAssembler.java
+++ src/main/java/com/estrellaticona/vetcare/pets/interfaces/rest/transform/PetResourceFromEntityAssembler.java
@@ -1,10 +1,14 @@
 package com.estrellaticona.vetcare.pets.interfaces.rest.transform;
 
+import java.text.SimpleDateFormat;
+
 import com.estrellaticona.vetcare.pets.domain.model.aggregates.Pet;
 import com.estrellaticona.vetcare.pets.interfaces.rest.resources.PetResource;
 
 public class PetResourceFromEntityAssembler {
     public static PetResource toResourceFromEntity(Pet entity) {
+        var format = new SimpleDateFormat("dd-MM-yyyy");
+        var date = format.format(entity.getCreatedAt());
         return new PetResource(
                 entity.getId(),
                 entity.getClientId(),
@@ -12,6 +16,6 @@ public class PetResourceFromEntityAssembler {
                 entity.getGender(),
                 entity.getAge(),
                 entity.getSpecie(),
-                entity.getCreatedAt());
+                date);
     }
 }
